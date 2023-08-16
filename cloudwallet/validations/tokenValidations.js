88af0ad95d0bde21b868eac71eff43c7f536e519
VAIVAL <muhammad.usama@vaivaltech.com>
2023-08-16 18:33:52 +0500

    first commit

--- cloudwallet/validations/tokenValidations.js
+++
@@ -1,9 +0,0 @@
-import * as yup from "yup";
-
-export const createTokenSchema = yup.object().shape({
-  symbol: yup.string().label("Symbol").required(),
-  decimalPlaces: yup.number().label("Decimal place").required(),
-  contractAddress: yup.string().label("Token contract").required(),
-  name: yup.string().label("Token name").required(),
-  tokenImage: yup.string().label("Image").required(),
-});
--- cloudwallet/validations/transferTokenValidations.js
+++
@@ -1,6 +0,0 @@
-import * as yup from "yup";
-
-export const transferTokenValidations = yup.object().shape({
-  amount: yup.string().label("Transfer amount").required(),
-  walletAddress: yup.string().label("Address").required(),
-});
